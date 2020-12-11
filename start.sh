7cf96ad45caba558a13d7e5e4be849a870fa5348
JerryHopper <hopper.jerry@gmail.com>
2020-12-11 15:32:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,8 @@ sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 # Always chown webroot for better mounting
 chown -Rf nginx.nginx /usr/share/nginx/html
 
+
+cron
+
 # Start supervisord and services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
