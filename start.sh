1b4ac38ea3e0c3054ff9f954efca4e5ed4326333
JerryHopper <hopper.jerry@gmail.com>
2020-12-11 16:24:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 # Always chown webroot for better mounting
 chown -Rf nginx.nginx /usr/share/nginx/html
 
+# export CF_Token=""
+# export CF_Account_ID=""
+# export CF_Zone_ID=""
+su - nginx -c "/home/nginx/.acme.sh/acme.sh  --issue -d ssl.dockbox.nl  -d '*.ssl.dockbox.nl'  --dns dns_cf"
 
 
 # Start supervisord and services
