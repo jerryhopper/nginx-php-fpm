02151faa3c5ed6b0e4f1b7c062d67a106793ddbe
JerryHopper <hopper.jerry@gmail.com>
2020-12-11 17:13:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ echo "CF_Zone_ID=$CF_Zone_ID"
 # export CF_Token=""
 # export CF_Account_ID=""
 # export CF_Zone_ID=""
-su - nginx -c "export CF_Token="$CF_Token" && export CF_Account_ID="$CF_Account_ID" && export CF_Zone_ID="$CF_Zone_ID" && /home/nginx/.acme.sh/acme.sh  --issue -d ssl.dockbox.nl  -d '*.ssl.dockbox.nl'  --dns dns_cf"
+su - nginx -c "export CF_Token="$CF_Token" && export CF_Account_ID="$CF_Account_ID" && export CF_Zone_ID="$CF_Zone_ID" && /home/nginx/.acme.sh/acme.sh --log --issue -d ssl.dockbox.nl  -d '*.ssl.dockbox.nl'  --dns dns_cf"
 
 
 # Start supervisord and services
